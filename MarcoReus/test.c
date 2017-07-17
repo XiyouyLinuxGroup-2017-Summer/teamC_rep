502d3f2a4f6f710d68bece7adf272ba7df43d704
YinJianxiang <YinjIanxiang123@gmail.com>
2017-07-17 12:02:12 +0800

    test.c
    about atoi

---
+++ MarcoReus/test.c
@@ -0,0 +1,18 @@
+/*************************************************************************
+    > File Name: test.c
+    > Author: YinJianxiang
+    > Mail: YinJianxiang123@gmail.com 
+    > Created Time: 2017年07月17日 星期一 11时41分25秒
+ ************************************************************************/
+
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(void) {
+	int a;
+
+	a = atoi("1234");
+
+	printf("%d\n",a);
+	return 0;
+}
