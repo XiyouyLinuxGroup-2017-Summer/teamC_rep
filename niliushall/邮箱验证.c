3f58a8b0544db51e524b3f4c2bdd50766381d973
niliusha <1459775963@qq.com>
2017-07-18 13:40:18 +0800

    update

---
+++ niliushall/邮箱验证.c
@@ -0,0 +1,63 @@
+/*************************************************************************
+	> File Name: 1.c
+	> Author: wangliang
+    > Email: sdwllinux@gmail.com
+	> Created Time: 2017年07月18日 星期二 11时19分39秒
+ ************************************************************************/
+
+#include <stdio.h>
+#include <string.h>
+int main (void)
+{
+    char mail[101];
+    int i, x, y, flag, len, flag_1;
+
+    while(scanf("%s", mail) != EOF){
+        x = 0;
+        y = 0;
+        flag = 0;
+        flag_1 = 0;
+        len = strlen(mail);
+        if(mail[0] == '@' || mail[0] == '.' || mail[len-1] == '@' || mail[len-1] == '.'){
+            flag_1 = 1;
+        }
+        for(i = 0; i < len; i++){
+            if(mail[i] == '@'){
+                x++;
+                flag = 1;
+                if(mail[i+1] == '.'){
+                    flag_1 = 1;
+                }
+                break;
+            }
+            if(mail[i] == '.'){
+                flag = 1;
+                if(mail[i+1] == '@'){
+                    flag_1 = 1;
+                }
+                break;
+            }
+        }
+
+        for(i++; i < len && flag; i++){
+            if(mail[i] == '.'){
+                y++;
+            }
+            if(mail[i] == '@')
+                x++;
+        }
+
+        if(x != 1 || !y){
+            flag_1 = 1;
+        }
+
+        if(flag_1){
+            printf("NO\n");
+        }
+        else{
+            printf("YES\n");
+        }
+    }
+
+    return 0;
+}
