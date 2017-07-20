3e3c14c227b7d6f0ef5c3f95321d3215d1743bee
hepangda <pangda@xiyoulinux.org>
2017-07-20 10:20:57 +0800

    update

--- README.md
+++ README.md
@@ -1,9 +1,15 @@
 # Team C's Repository
+
+## Blog & Github
+
 chudongfang CSDN  : http://blog.csdn.net/chudongfang2015
 
 chudongfang GitHub: https://github.com/chudongfang
 
 yinjianxiang CSDN :http://blog.csdn.net/yinjianxiang
 
-yinjianxiang GitHub:http://blog.csdn.net/yinjianxiang
+yinjianxiang GitHub:https://github.com/yinjianxiang
 
+louzehao CSDN : http://blog.hepangda.com/
+
+louzehao Github : https://github.com/hepangda
--- pangda/src/exp.cpp
+++ pangda/src/exp.cpp
@@ -1,20 +1,27 @@
-#include<iostream>
-#include<cstdio>
-using namespace std;
-
+#include<stdio.h>
+#define input(ret) {char c; ret = 0; while((c = getchar()) < '0' || c > '9'); while (c >= '0' && c <= '9') ret = ret * 10 + (c - '0'), c = getchar();}
 int main() {
-	int cas__;
-	cin >> cas__;
-	getchar();
-	while (cas__--) {
-        wchar_t t;
-		int cnt = 0;
-		while ((t = getwchar()) != '\n') {
-			if (t > 128)
-				cnt++;
+	int cas;
+	input(cas);
+	while (cas--) {
+		char tmp, t[1001];
+		int tsz = 0;
+		while (tmp = getchar()) {
+			if (tmp == ' ' || tmp == '\n') {
+				for (int i = tsz - 1; i >= 0; i--)
+					putchar(t[i]);
+				tsz = 0;
+
+				if (tmp == '\n') {
+					putchar('\n');
+					break;
+				} else {
+					putchar(' ');
+				}
+			} else {
+				t[tsz++] = tmp;
+			}
 		}
-		cnt /= 2;
-		cout << cnt << endl;
 	}
 	return 0;
-}w
+}
