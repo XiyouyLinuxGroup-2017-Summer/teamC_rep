ecb5af5e8411b3eeb7179b7757801f1e97056aec
hepangda <pangda@xiyoulinux.org>
2017-07-22 11:13:21 +0800

    ls 1.0

--- pangda/ls/ls.cpp
+++ pangda/ls/ls.cpp
@@ -1,10 +1,10 @@
 #include<pangda/ls.h>
 
 int main(int argc, char *argv[]) {
-    param_t param = set_param(argc, argv);
+    param_t param = set_param(argc, argv);         //分析带有的参数
     if (param.path[param.path.size() - 1] != '/')
-        param.path += '/';
-    auto fli = build_filelist(param);
-    output(param, fli);
+        param.path += '/';  //若要显示的目录不以/结尾，那么手动在之后加上这个属性
+    auto fli = build_filelist(param);   //根据参数构建文件列表
+    output(param, fli);     //按指定参数形式输出文件列表
     return 0;
 }
