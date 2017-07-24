125b18e591ea4840ecdaed60497d6befa78e1e73
hepangda <pangda@xiyoulinux.org>
2017-07-24 09:46:44 +0800

    update

--- pangda/src/exp.cpp
+++ pangda/src/exp.cpp
@@ -1,36 +1,30 @@
-#include<cstdio>
-#include<stack>
+#include<iostream>
 
 using namespace std;
+using llong = long long;
+
+llong gcd(const llong a, const llong b) {
+	return b ? gcd(b, a % b) : a;
+}
 
 int main() {
-	int cas;
-	scanf("%d", &cas);
-	getchar();
-	while (cas--) {
-		stack<char> S;
-		char t;
-		while (t = getchar()) {
-			if (t == ' ') {
-				while (!S.empty()) {
-					t = S.top();
-					S.pop();
-					putchar(t);
-				}
-				putchar(' ');
-			} else if (t == '\n') {
-				while (!S.empty()) {
-					t = S.top();
-					S.pop();
-					putchar(t);
-				}
-				putchar('\n');
-				break;
-			} else {
-				S.push(t);
-			}
-		}
 
+	int cas;
+	cin >> cas;
+	while (cas--) {
+		llong nume1, nume2, deno1, deno2;
+		char trash;
+		cin >> nume1 >> trash >> deno1 >> nume2 >> trash >> deno2;
+		llong f = gcd(nume1, deno1);
+		nume1 /= f; deno1 /= f;
+		f = gcd(nume2, deno2);
+		nume2 /= f; deno2 /= f;
+		llong x = (nume1 * nume2) / gcd(nume1, nume2),
+			  y = gcd(deno1, deno2);
+		if (x % y)
+			cout << x << "/" << y << endl;
+		else
+			cout << x / y << endl;
 	}
 	return 0;
 }
