ced031d01a32dc19b210c64de724c78ebbef448c
yinjianxiang <YinJianxiang@gmail.com>
2017-07-30 15:29:32 +0800

    ^_^

--- MarcoReus/jincheng/daemon.c
+++ MarcoReus/jincheng/daemon.c
@@ -36,7 +36,7 @@ int init_daemon(void) {
 
     /*创建一个新的进程组，在这个新的组中，子进程成为进程组的首进程，以使该进程脱离所有终端*/
     setsid();
-    
+    /*再创建一个子进程，退出父进程，保证该进程不是进程组长，同时让该进程无法再打开一个新的终端*/    
     pid = fork();
     if(pid > 0) {
         exit(0);
@@ -44,12 +44,16 @@ int init_daemon(void) {
         return -1;
     }
 
+    /*关闭所有从父进程继承的不再需要的文件描述符*/
     for(i = 0;i < NOFILE;close(i++));
 
+    /*改变工作目录，使得进程不与任何文件系统联系*/
     chdir("/");
 
+    /*将文件屏蔽字设置为0*/
     umask(0);
 
+    /*忽略SIGCHLD信号*/
     signal(SIGCHLD,SIG_IGN);
 
     return 0;
