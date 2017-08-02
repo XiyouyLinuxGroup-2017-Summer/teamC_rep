fbfa80a714e7db9fb42b2acbbc01b1eae01e1921
hepangda <pangda@xiyoulinux.org>
2017-08-02 08:37:20 +0800

    ALGORITHM

--- pangda/src/ago.cpp
+++ pangda/src/ago.cpp
@@ -1,66 +1,41 @@
 #include<cstdio>
 #include<cstring>
-#include<algorithm>
 using namespace std;
 
-int path[88][88], vis[88][88], p, q, cnt;
-bool flag;
+int r__, c__;
+char g[30][30];
+bool vis[26];
+int result;
 
-int dx[8] = {-1, 1, -2, 2, -2, 2, -1, 1};
-int dy[8] = {-2, -2, -1, -1, 1, 1, 2, 2};
+const int DIR[4][2] = { -1, 0, 1, 0, 0, 1, 0, -1};
 
-bool judge(int x, int y)
-{
-    if(x >= 1 && x <= p && y >= 1 && y <= q && !vis[x][y] && !flag)
-        return true;
-    return false;
-}
-
-void DFS(int r, int c, int step)
-{
-    path[step][0] = r;
-    path[step][1] = c;
-    if(step == p * q)
-    {
-        flag = true;
-        return ;
-    }
-    for(int i = 0; i < 8; i++)
-    {
-        int nx = r + dx[i];
-        int ny = c + dy[i];
-        if(judge(nx,ny))
-        {
-
-            vis[nx][ny] = 1;
-            DFS(nx,ny,step+1);
-            vis[nx][ny] = 0;
-        }
+void dfs(int lastx, int lasty, int depth) {
+    if (depth > result)
+        result = depth;
+    for (int i = 0; i < 4; i++) {
+        int thisx = lastx + DIR[i][0],
+            thisy = lasty + DIR[i][1];
+        
+        if (thisx >= r__ || thisy >= c__ || thisx < 0 || thisy < 0 || vis[g[thisx][thisy] - 'A'])
+            continue;
+        vis[g[thisx][thisy] - 'A'] = true;
+        dfs(thisx, thisy, depth + 1);
+        vis[g[thisx][thisy] - 'A'] = false;
     }
 }
 
-int main()
-{
-    int i, j, n, cas = 0;
-    scanf("%d",&n);
-    while(n--)
-    {
-        flag = 0;
-        scanf("%d%d",&p,&q);
-        memset(vis,0,sizeof(vis));
-        vis[1][1] = 1;
-        DFS(1,1,1);
-        printf("Scenario #%d:\n",++cas);
-        if(flag)
-        {
-            for(i = 1; i <= p * q; i++)
-                printf("%c%d",path[i][1] - 1 + 'A',path[i][0]);
+int main() {
+    while (~ scanf("%d%d", &r__, &c__)) {
+        memset(vis, 0, sizeof(vis));
+        result = 0;
+        for (int i = 0; i < r__; i++) {
+            for (int j = 0; j < c__; j++) {
+                scanf(" %c", &g[i][j]);
+            }
         }
-        else
-            printf("impossible");
-        printf("\n");
-        if(n != 0)
-            printf("\n");
+        vis[g[0][0] - 'A'] = true;
+        dfs(0, 0, 1);
+        printf("%d\n", result);
     }
     return 0;
 }
